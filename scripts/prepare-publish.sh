d2bae62b9becd40ee5c1dd4fcc7911f59e9444ad
Michael Ira Krufky <mkrufky@linuxtv.org>
2017-02-14 15:39:13 -0500

    scripts: fix scripts/prepare-publish.sh
    Signed-off-by: Michael Ira Krufky <mkrufky@linuxtv.org>

--- scripts/prepare-publish.sh
+++ scripts/prepare-publish.sh
@@ -8,10 +8,14 @@ cd _stage
 git clone .. node-dvbtee
 cd node-dvbtee
 
+scripts/prepare-build.sh
+
 sed -i s/"scripts\/prepare-build.sh; scripts\/configure-build.sh"/"scripts\/configure-build.sh"/1 package.json
 sed -i s/"^libdvbtee"/""/1 .gitignore
 
-scripts/prepare-build.sh
+cd libdvbtee
+sed -i s/"^libdvbpsi\/"/""/1 .gitignore
+cd ..
 
 npm publish
 
