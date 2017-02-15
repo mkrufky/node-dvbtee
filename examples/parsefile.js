8622b88aa53cae4a868ef0e48d358b3bb21036ce
Michael Ira Krufky <mkrufky@linuxtv.org>
2017-02-15 07:55:14 -0500

    move example run.js into examples/
    Signed-off-by: Michael Ira Krufky <mkrufky@linuxtv.org>

--- examples/parsefile.js
+++ examples/parsefile.js
@@ -1,22 +1,24 @@
-var dvbtee = require('./build/Release/dvbtee');
-
-var fs = require('fs');
+var fs = require('fs')
+var dvbtee = require('bindings')('dvbtee.node')
 
 var parser = new dvbtee.Parser
 
-parser.listenTables(function(tableId, tableName, d) {
+parser.listenTables(function(tableId, tableName, data) {
+
   console.log('table id: ' + tableId,
               '\ntable name: ' + tableName,
-              '\ntable data:\n', d)
+              '\ntable data:\n', JSON.stringify(data, null, 2))
 })
 
 fs.readFile('sample.ts', function(err, buf) {
-    console.log('pushing ' + buf.length + ' bytes');
+
+    console.log('pushing ' + buf.length + ' bytes')
+
     var ret = parser.push(buf, buf.length, function(err, status) {
+
       if (err)
           console.log(err)
       else
           console.log('complete: status = ' + status)
-      process.exit()
     })
 })
