d12e91cb912c622b2c893444c2645e2615246046
Michael Ira Krufky <mkrufky@linuxtv.org>
2017-03-12 09:41:55 -0400

    package.json: allow git users to use the package
    Signed-off-by: Michael Ira Krufky <mkrufky@linuxtv.org>

--- scripts/prepare-publish.sh
+++ scripts/prepare-publish.sh
@@ -11,7 +11,7 @@ cd node-dvbtee
 scripts/prepare-build.sh
 node scripts/configure-build.js
 
-sed -i s/"scripts\/prepare-build.sh && node scripts\/configure-build.js"/""/1 package.json
+sed -i s/"npm install mkdirp symlink-or-copy && scripts\/prepare-build.sh && node scripts\/configure-build.js"/""/1 package.json
 sed -i s/"^libdvbtee"/""/1 .gitignore
 
 cd libdvbtee
