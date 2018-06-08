fc747516c690cc0df9e05d081241087abebd8df5
Michael Ira Krufky <mkrufky@gmail.com>
2018-06-07 23:48:45 -0400

    scripts: remove libdvbtee / libdvbpsi preparation
    Signed-off-by: Michael Ira Krufky <mkrufky@gmail.com>

--- scripts/prepare-publish.sh
+++ scripts/prepare-publish.sh
@@ -8,23 +8,7 @@ cd _stage
 git clone .. node-dvbtee
 cd node-dvbtee
 
-node scripts/prepare-build.js
-node scripts/configure-build.js
-
-sed -i s/"npm install git-clone mkdirp rimraf && node scripts\/prepare-build.js && node scripts\/configure-build.js"/""/1 package.json
 sed -i s/"^libdvbtee"/""/1 .gitignore
-
-cd libdvbtee
-sed -i s/"^libdvbpsi\/"/""/1 .gitignore
-sed -i s/"^usr\/"/""/1 .gitignore
-sed -i s/"^config.h"/""/1 .gitignore
-cd libdvbpsi
-sed -i s/"^config.h"/""/1 .gitignore
-cd ..
-cd libdvbtee
-sed -i s/"static bool hello = false"/"static bool hello = true"/1 parse.cpp
-cd ..
-cd ..
 
 npm publish
 
